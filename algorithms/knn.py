8a83890bca084307efac74d6ce86db1bf3c476c2
Eric Schanet <eric.schanet@cern.ch>
2021-08-06 16:12:15 +0200

    docs: adding docstrings to knn class

--- algorithms/knn.py
+++ algorithms/knn.py
@@ -5,16 +5,43 @@ from scipy.spatial import distance
 class KNN:
 
     def __init__(self, k: int):
+        """Initialize the KNN
+
+        Args:
+            k (int): number of clusters
+        """
         self.k = k
 
     def fit(self, X: np.ndarray, y: np.ndarray):
+        """Fit the model to the training data/labels
+
+        Args:
+            X (np.ndarray): training data
+            y (np.ndarray): training class labels
+        """        
         self.X_train = X
         self.y_train = y
 
     def predict(self, X: np.ndarray) -> np.ndarray:
+        """Predict the class of a set of data
+
+        Args:
+            X (np.ndarray): Datapoints to predict class labels for
+
+        Returns:
+            np.ndarray: The predicted class labels
+        """        
         return np.array([self._predict(x) for x in X])
     
     def _predict(self, x: np.ndarray) -> int:
+        """Helper method to predict class labels for samples X
+
+        Args:
+            x (np.ndarray): Single sample to predict class label for
+
+        Returns:
+            int: predicted class label
+        """        
         # p=2 minkowski metric is euclidian distance
         distances = [distance.minkowski(x, x_train, p=2) for x_train in self.X_train]
         # get k nearest neighbors and return the majority class vote
